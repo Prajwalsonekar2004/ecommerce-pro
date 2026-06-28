e993fa93b1cc65fcb45cea06ee7d52bdf2be8f0b
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-06-28 07:51:14 +0530

    Add product type and improve data structure

---
+++ types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id:number;
+    name:string;
+    slug:string;
+    description:string;
+    price:number;
+    category:string;
+    brand:string;
+    stock:number;
+    rating:number;
+    image:string;
+}
