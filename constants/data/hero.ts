6d9e0b366289e0ef77fce095bdde91681b148a29
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-07-02 19:06:28 +0530

    Create scalable homepage layout and  hero architecture

---
+++ constants/data/hero.ts
@@ -0,0 +1,9 @@
+export const heroSlides = [
+  {
+    id: 1,
+    image: "/images/hero/hero-1.jpg",
+    title: "Premium Fashion Collection",
+    Subtitle: "New Season",
+    buttonText: "Shop Now",
+  },
+];
