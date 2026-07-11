aa6817d13341e4ff0ddb96c5335a612d147a1536
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-07-11 13:44:22 +0530

    Refine hero layout and initialize Swiper carousel

--- components/home/hero/HeroCarousel.tsx
+++ components/home/hero/HeroCarousel.tsx
@@ -1,3 +1,15 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swi"
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
+export default function HeroCarousel() {
+  return (
+    <Swiper>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+    </Swiper>
+  );
+}
