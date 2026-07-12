8baf0a398113de69808cd34fa9c79c76b4e2c6e7
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-07-12 13:16:46 +0530

    Fix product grid props and separate grid from carousel architecture

--- components/product/ProductGrid.tsx
+++ components/product/ProductGrid.tsx
@@ -2,10 +2,12 @@ import { Product } from "@/types/product";
 import ProductCard from "./ProductCard";
 
 type ProductGridProps = {
-  product: Product;
+  products: Product[];
 };
 
-export default function ProductGrid({ products }: ProductGridProps) {
+export default function ProductGrid({
+   products,
+ }: ProductGridProps) {
   return (
     <section className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
       {products.map((product) => (
--- components/product/ProductSlider.tsx
+++ components/product/ProductSlider.tsx
@@ -1,12 +1,44 @@
-"use-client";
+"use client";
 
-import { Swiper } from "swiper/types";
+import { ReactNode } from "react";
+import { Swiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
 import "swiper/css";
+import "swiper/css/navigation";
 
 type ProductSliderProps = {
-  children: React.ReactNode;
+  children: ReactNode;
 };
 
-export default function ProductSlider({ children }: ProductSliderProps) {
-  return <div className="overflow-hidden">{children}</div>;
+export default function ProductSlider({
+     children,
+    }: ProductSliderProps) {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      navigation
+      spaceBetween={24}
+      slidesPerView={1.2}
+      breakpoints={{
+        640: {
+          slidesPerView: 2,
+        },
+
+        768: {
+          slidesPerView: 2.5,
+        },
+
+        1024: {
+          slidesPerView: 3,
+        },
+
+        1280: {
+          slidesPerView: 4,
+        },
+      }}
+    >
+      {children}
+    </Swiper>
+  );
 }
