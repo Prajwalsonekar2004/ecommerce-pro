984482c82969cb4073fbb0739822d5c45a67870c
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-08-18 18:14:56 +0530

    add better auth client

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -2,3 +2,32 @@ import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { GET, POST } = toNextJsHandler(auth);
+
+
+// fetch("/api/auth/email-otp/send-verification-otp", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     email: "test@example.com",
+//     type: "sign-in",
+//   }),
+// }).then(async (response) => {
+//   console.log("STATUS:", response.status);
+//   console.log("RESPONSE:", await response.text());
+// });
+
+// fetch("/api/auth/sign-in/email-otp", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     email: "test@example.com",
+//     otp: "240176",
+//   }),
+// }).then(async (response) => {
+//   console.log("STATUS:", response.status);
+//   console.log("RESPONSE:", await response.text());
+// });
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
+import { emailOTP } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        console.log("OTP:", { email, otp, type });
+      },
+    }),
+  ],
 });
