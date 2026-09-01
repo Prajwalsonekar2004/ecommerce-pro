076fbf71d4cf34f0699e17dd70601655ad93448a
SudhanshuShiwarkar <sudhanshu.shiwarkar@talentserv.co.in>
2026-09-01 16:17:07 +0530

    fix production auth and homepage data

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import NewArrivals from "@/components/home/NewArrivals";
 // import TrendingCollection from "@/components/home/TrendingCollection";
 import Newsletter from "@/components/home/Newsletter";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main>
